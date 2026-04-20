0be8b8cc99db560ea4dc453d201de2681080c696
dragonnegro190285 <dragonnegro190285@users.noreply.github.com>
2026-04-19 19:14:44 -0600

    Reactivar persistSession y detectSessionInUrl para mantener sesión entre páginas

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,7 +6,7 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: false,
-    persistSession: false,
-    detectSessionInUrl: false
+    persistSession: true,
+    detectSessionInUrl: true
   }
 })
